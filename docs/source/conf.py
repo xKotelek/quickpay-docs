4cb1b98454cdccd7029e97acf064d8d63e9d5528
xKotelek <contact@kotelek.dev>
2024-03-17 11:35:15 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'quickpay'
+copyright = '2023-2024, xKotelek'
+author = 'xKotelek'
 
 release = '0.1'
 version = '0.1.0'
